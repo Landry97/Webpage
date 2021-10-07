6e32920fb54538d0c60394d8aa0ca8ae495f40b0
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-10-07 12:10:21 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -15,7 +15,7 @@ mybutton = document.getElementById("myBtn");
 // [When the user scrolls down 20px from the top of the document, show the button]
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
        mybutton.style.display = "block";} 
        else {
        mybutton.style.display = "none";}
@@ -24,5 +24,4 @@ function scrollFunction() {
 function topFunction() {
  document.body.scrollTop = 0; // For Safari
  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-
 };
