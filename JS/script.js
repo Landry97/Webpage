4d837e15183b874b8e0307b740f424bc95957d21
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-10-09 03:24:30 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,7 @@ menu.addEventListener('click', () => {
       navMenu.classList.toggle('show');
       navUL.classList.toggle('show');
       address.classList.toggle('show');
-      activePage.classList.toggle('activePage2');
+      activePage.className.toggle('activePage2');
 }); 
 
 menu.addEventListener('click', () => {
