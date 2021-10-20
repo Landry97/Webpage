9f2b14f2d532564a4a6c4c9b2d062ff043b551b6
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-10-20 18:06:16 +0200

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -3,6 +3,7 @@ let slideIndex = 1;
 function openLightbox() {
   document.getElementById('Lightbox').style.display = 'flex';
   document.getElementById('rowid').style.display = 'none';
+  document.getElementById('madebyid').style.display = 'none';
   document.getElementsByTagName("Footer")[0].style.display = "none";
   document.getElementsByTagName("header")[0].style.display = "none";
 var mq = window.matchMedia( "(max-width: 950px)" );
@@ -14,6 +15,7 @@ if (mq.matches) {
 function closeLightbox() {
   document.getElementById('Lightbox').style.display = 'none'; 
   document.getElementById('rowid').style.display = 'flex';
+  document.getElementById('madebyid').style.display = 'flex';
   document.getElementsByTagName("Footer")[0].style.display = "flex";
   document.getElementsByTagName("header")[0].style.display = "flex";
 var mq = window.matchMedia( "(max-width: 950px)" );
