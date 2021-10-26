afe1b0d420726e6401f651c0507ba9cc3869d3e4
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-10-26 23:21:20 +0200

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,7 +1,8 @@
 let slideIndex = 1;
 
 function openLightbox() {
-  document.getElementsByTagName('header')[0].style.overflow ="hidden";
+  document.getElementsByTagName('body')[0].style.overflow ="hidden";
+  document.getElementsByTagName('body')[0].style.height ="100vh";
   document.getElementById('Lightbox').style.display = 'flex';
   document.getElementById('rowid').style.display = 'none';
   document.getElementById('info').style.display = 'none';
@@ -15,7 +16,8 @@ if (mq.matches) {
 };
 
 function closeLightbox() {
-  document.getElementsByTagName('header')[0].style.overflow ="";
+  document.getElementsByTagName('body')[0].style.overflow ="";
+  document.getElementsByTagName('body')[0].style.height ="";
   document.getElementById('Lightbox').style.display = 'none'; 
   document.getElementById('rowid').style.display = 'flex';
   document.getElementById('info').style.display = 'flex';
