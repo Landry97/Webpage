2f7296c2982f7855c362ef73c6396974b3e77156
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-10-26 23:32:03 +0200

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -9,7 +9,7 @@ function openLightbox() {
   document.getElementById('menu-toggle').style.display = 'none';
   document.getElementById('madebyid').style.display = 'none';
   document.getElementsByTagName("Footer")[0].style.display = "none";
-  document.getElementsByClassName('header-title').style.position = 'relative';
+  document.getElementByClassName("header-title").style.position = 'relative';
   
 var mq = window.matchMedia( "(max-width: 950px)" );
 if (mq.matches) {
@@ -18,7 +18,7 @@ if (mq.matches) {
 };
 
 function closeLightbox() {
-  document.getElementsByClassName('header-title').style.position = '';
+  document.getElementByClassName("header-title").style.position = '';
   document.getElementsByTagName('body')[0].style.overflow ="";
   document.getElementsByTagName('body')[0].style.height ="";
   document.getElementById('Lightbox').style.display = 'none'; 
