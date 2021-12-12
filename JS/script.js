c89ae8dc156eb539c552a0cc224e31235f11b70f
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-12 20:28:44 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,8 @@ var extern = document.getElementsByTagName("link")[5].import;
 var absatz = extern.getElementsByTagName("p")[0];
 
 window.addEventListener("load", function() {
-document.getElementsByTagName("html")[0].replaceChild(extern.getElementsByTagName("header")[0],
-document.getElementsByTagName("header")[0]);
+document.getElementsByTagName("html")[0].replaceChild(extern.getElementsByTagName("body")[0],
+document.getElementsByTagName("body")[0]);
 }, false);
 
 
