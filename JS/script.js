7e8b2973ec55685dc7d8071b61cf92c42220842b
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-12 20:15:48 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 var extern = document.getElementsByTagName("link")[5].import;
+var absatz = extern.getElementsByTagName("header")[0];
 
 window.addEventListener("load", function() {
 document.getElementsByTagName("html")[0].replaceChild(extern.getElementsByTagName("header")[0],
